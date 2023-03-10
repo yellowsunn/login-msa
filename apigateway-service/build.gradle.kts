2d86bf35d1eab0d37673d1e1a1d32269d2423533
yellowsunn <chohankook95@gmail.com>
2023-03-10 15:08:31 +0900

    apigateway dev setting

--- apigateway-service/build.gradle.kts
+++ apigateway-service/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.google.cloud.tools.jib.gradle.JibExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "3.0.3"
     id("io.spring.dependency-management") version "1.1.0"
+    id("com.google.cloud.tools.jib") version "3.3.1"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -40,4 +42,13 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configure<JibExtension> {
+    from {
+        image = "eclipse-temurin:17-jre-alpine"
+    }
+    container {
+        ports = listOf("8080")
+    }
 }
